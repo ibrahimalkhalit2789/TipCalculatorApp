a2cbd24f4f24c3ef24d325df9cd0e57d95f7a904
ibrahimalkhalit2789 <ibrahimalkhalit2789@nycstudents.net>
2023-09-26 12:12:47 -0400

    Final version

---
+++ src/tipCalculator.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class tipCalculator {
+    public static void main(String[]args) {
+
+        //INIT
+        Scanner scan = new Scanner(System.in);
+
+        //OUTPUT VARS
+        double tipAmt;
+        double billInclTip;
+        double tipPerPerson;
+        double totalPerPerson;
+
+        //HEADING
+        System.out.println("_________________");
+        System.out.println("| TIP CALCULATOR |");
+        System.out.println("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾");
+        System.out.println();
+
+        //INPUT
+        System.out.println("BILL: ");
+        double totalBill = scan.nextDouble();
+        System.out.println();
+
+        System.out.println("TIP PERCENT: ");
+        int tipPercent = scan.nextInt();
+        System.out.println();
+
+        System.out.println("NO. OF PEOPLE IN PARTY: ");
+        int pplInParty = scan.nextInt();
+        System.out.println();
+
+        System.out.println();
+        System.out.println();
+
+        //CALCULATION
+        tipAmt = (totalBill * tipPercent);
+        billInclTip = (totalBill + tipAmt);
+        tipPerPerson = (tipAmt / pplInParty);
+        totalPerPerson = (billInclTip / pplInParty);
+
+        //OUTPUT
+        System.out.println("TOTAL TIP AMOUNT: " + tipAmt);
+        System.out.println("TOTAL BILL INCL. TIP: " + billInclTip);
+        System.out.println("TIP PER PERSON: " + tipPerPerson);
+        System.out.println("TOTAL PER PERSON: " + totalPerPerson);
+    }
+}
